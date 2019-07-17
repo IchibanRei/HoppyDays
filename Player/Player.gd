bb81f66207f9a5b3a2775dfafe5a1f73023696a5
Rey <ichibanrei@gmail.com>
2019-07-17 07:25:56 -0700

    Adding artwork
    Added level and player artwork and animation

---
+++ Player/Player.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+var motion = Vector2(0, 0)
+
+const SPEED = 1500
+const GRAVITY = 150	
+const UP = Vector2(0, -1)
+const JUMP_SPEED = 3500
+
+signal animate
+
+func _process(delta):
+	apply_gravity()
+	jump()
+	move()
+	animate()
+	move_and_slide(motion, UP)
+	
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+	elif is_on_ceiling():
+		motion.y = 1
+	else:
+		motion.y += GRAVITY
+
+func move():
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right")and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+
+func jump():
+	if Input.is_action_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
+func animate():
+	emit_signal("animate", motion)
